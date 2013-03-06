3e9100850ef2ec0e34485e40a6793164d33f787f
Ryan Eastridge <ryan@eastridge.me>
2013-03-06 10:37:11 -0800

    Initial commit

---
+++ application.js
@@ -0,0 +1,21 @@
+window.Application = {};
+
+Application.View = Backbone.View.extend({
+  render: function() {
+    var context = this.model ? this.model.attributes : {},
+        output = this.options.template(context);
+    this.$el.html(output);
+  }
+});
+
+$(function() {
+  var model = new Backbone.Model({
+    greeting: 'Hello'
+  });
+  var indexView = new Application.View({
+    template: Handlebars.templates['index'],
+    model: model
+  });
+  indexView.render();
+  $('body').append(indexView.el);
+});
